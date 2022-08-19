a2067c69588709702021ba896fefb9bff664ccbe
Daniel Dantas da Silva <daniel.dantas.98@gmail.com>
2022-08-19 15:28:10 -0300

    Wordle finalizado

--- js-index.js
+++ js-index.js
@@ -5,7 +5,7 @@ let elLinha = document.getElementById(`linha${linha}`)
 let elItens = elLinha.querySelectorAll('.caixinha')
 var caixinha = 0
 var palavra = []
-const palavraDoDia = 'AMORA'
+const palavraDoDia = 'OSSOS'
 
 // inserindo uma informação no meu HTML
 // elItem1.innerHTML = 'A'
@@ -13,84 +13,127 @@ const palavraDoDia = 'AMORA'
 // elItem1.classList.add('preenchendo')
 
 const ouvinteDeTeclas = (event) => {
-    let tecla = event.key.toUpperCase();    
+    let tecla = event.key.toUpperCase();
     const alfabeto = [
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'I', 'L',
-        'M','N','O','P,','Q','R','S','T','U','U','V','W','X','Y','Z'
+        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'U', 'V', 'W', 'X', 'Y', 'Z'
     ];
-// Pula para a proxima linha, após ter 5 caracteres naquela linha, e chama a função para validar os dados 
-        if (tecla == 'ENTER' && caixinha == 4 && linha <= 6){
-            if (linha < 6){
-                validaPalavra(palavra)
-                linha += 1
-                elLinha = document.getElementById(`linha${linha}`)
-                elItens = elLinha.querySelectorAll('.caixinha')
-                caixinha = 0  
-                palavra = []              
-            }
-            else if (linha == 6){
-                validaPalavra(palavra)
+    // Insere um caractere na caixinha
+    if (alfabeto.includes(tecla)) {
+        if (palavra.length < 5) {
+            elItens[caixinha].innerHTML = tecla     
+            palavra.push(tecla)       
+            pintaPreenchendo(caixinha)
+            if (palavra.length < 5) {
+                caixinha += 1                
+            } else if (palavra.lenght == 5) {
+                palavra[caixinha] = tecla
+                pintaPreenchendo(caixinha)
+                console.log('aqui')
             }
         }
-// Apaga um caractere das caixinha
-        if (tecla == 'BACKSPACE'){
-            palavra.pop()
-            elItens[caixinha].innerHTML = ''
-            elItens[caixinha].classList.add('vazio')
-            if (caixinha > 0){
-                caixinha -= 1}
-        } 
-// Insere um caractere na caixinha
-        if (alfabeto.includes(tecla)) {
-            if (caixinha < 5){        
-            elItens[caixinha].innerHTML = tecla
-                if (caixinha < 4){
-                    palavra.push(tecla)
-                    elItens[caixinha].classList.add('preenchendo')
-                    elItens[caixinha].classList.remove('vazia')
-                    caixinha += 1
-                } else if (caixinha == 4){
-                    palavra[caixinha] = tecla
-                    elItens[caixinha].classList.add('preenchendo')
-                    elItens[caixinha].classList.remove('vazia')
-                }
-                }
+    }
+    // Pula para a proxima linha, após ter 5 caracteres naquela linha, e chama a função para validar os dados 
+    if (tecla == 'ENTER' && palavra.length == 5 && linha <= 6) {
+        if (linha < 6) {
+            validaPalavra(palavra)
+            linha += 1
+            elLinha = document.getElementById(`linha${linha}`)
+            elItens = elLinha.querySelectorAll('.caixinha')
+            caixinha = 0
+            palavra = []
         }
-    
-    
+        else if (linha == 6) {
+            validaPalavra(palavra)
+        }
+    }
+    // Apaga um caractere das caixinha
+    if (tecla == 'BACKSPACE') {
+        console.log(caixinha)
+        palavra.pop()
+        elItens[caixinha].innerHTML = ''
+        pintavazia(caixinha)
+        if (caixinha > 0) {
+            caixinha -= 1
+        }
+    }
 }
 
 // Valida se a letra esta na posição certa e se a letra existe na palavra
 const validaPalavra = (palavra) => {
     var validaPalavraDoDia = palavraDoDia
-    palavra.join('')
     if (palavra.join('') == palavraDoDia) {
         caixinha = 0
-        for (n of elItens){
+        for (n of elItens) {
             n.classList.add('verde')
             n.classList.remove('vazia', 'preenchendo')
         }
     }
-// Percorre a palavraDoDia considerando o seu indice.
-    for (indice in palavraDoDia){     
-// Compara letra por letra da palavraDoDia com a palavra digita pelo usuario utilizando seu indice como "localizador"
-        if (validaPalavraDoDia.indexOf(palavra[indice]) == indice){
+    // Percorre a palavraDoDia considerando o seu indice.
+    palavra = palavra.join('')
+    for (indice in palavraDoDia) {
+     //debugger
+        const regex = new RegExp(palavra[indice], "gi")
+        // Compara letra por letra da palavraDoDia com a palavra digita pelo usuario utilizando seu indice como "localizador"
+        if (validaPalavraDoDia.indexOf(palavra[indice]) == indice) {
             validaPalavraDoDia = validaPalavraDoDia.replace(palavra[indice], '*')
-            palavra[indice] = '*'        
-            elItens[indice].classList.add('verde')
+            palavra = palavra.replace(palavra[indice], '*')
+            pintaVerde(indice)
+        } else if (!validaPalavraDoDia.includes(palavra[indice])) {
+            elItens[indice].classList.add('cinza')
             elItens[indice].classList.remove('vazia', 'preenchendo')
-            console.log(palavra)
-        } else if (validaPalavraDoDia.indexOf(palavra[indice]) < palavra.indexOf[validaPalavraDoDia]) {          
-
+        } else if (palavra.match(regex).length > validaPalavraDoDia.match(regex).length) {
+            pintaCinza(indice)
+            palavra = palavra.replace(palavra[indice], '*')
+        } else if (
+            (validaPalavraDoDia.indexOf(palavra[indice]) < palavra.indexOf([palavra[indice]]))
+            ||
+            ((validaPalavraDoDia.includes(palavra[indice]) && (palavra.match(regex).length >= validaPalavraDoDia.match(regex).length)))
+            ||
+            (validaPalavraDoDia.indexOf(palavra[indice]) > palavra.indexOf([palavra[indice]]))
+        ) {
+            console.log(indice)
+            if (palavra[indice]==palavraDoDia[indice]){
+                validaPalavraDoDia = validaPalavraDoDia.replace(palavra[indice], '*')
+                palavra = palavra.replace(palavra[indice], '*')
+                pintaVerde(indice)
+            } else {
+                validaPalavraDoDia = validaPalavraDoDia.replace(palavra[indice], '*')
+                palavra = palavra.replace(palavra[indice], '*')
+                pintaAmarelo(indice)
+            }       
+            
         }
     }
 }
 
 document.body.addEventListener('keydown', ouvinteDeTeclas)
-
-
-
-
+//(validaPalavraDoDia.indexOf(palavra[indice]) < palavra.indexOf([palavra[indice]]))
 // SO VOU UTILIZAR NO MATCH
 // const regex = new RegExp(palavra.join('')[indice], "gi")   
 
+
+const pintaAmarelo = (indice) => {
+    elItens[indice].classList.add('amarelo')
+    elItens[indice].classList.remove('vazia', 'preenchendo')
+}
+
+const pintaVerde = (indice) => {
+    elItens[indice].classList.add('verde')
+    elItens[indice].classList.remove('vazia', 'preenchendo')
+}
+
+const pintaCinza = (indice) => {
+    elItens[indice].classList.add('cinza')
+    elItens[indice].classList.remove('vazia', 'preenchendo')
+}
+
+const pintaPreenchendo = (caixinha) => {
+    elItens[caixinha].classList.add('preenchendo')
+    elItens[caixinha].classList.remove('vazia')
+}
+
+const pintavazia = (caixinha) => {
+    elItens[caixinha].classList.add('vazia')
+    elItens[caixinha].classList.remove('preenchendo')
+}
